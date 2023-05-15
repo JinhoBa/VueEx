79ff65bec4e64b1f2ad725d1e655ef71c026d529
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2023-05-15 16:33:15 +0900

    add week10

---
+++ vue_week10/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!--데이터 바인딩-->
+    <h1>{{ title }}..</h1>
+    <!--vhtml-->
+    <h2>{{ htmlString }}</h2>
+    <h2 v-html="htmlString"></h2>
+    <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+    <label><input type="checkbox" value="부산" v-model="checked">부산</label>
+    <label><input type="checkbox" value="제주" v-model="checked">제주</label>
+    <br>
+    <span>체크한 지역: {{ checked }}</span>
+  </div>
+</template>
+
+<script>
+export default ({
+  data () {
+    return {
+      title: 'Vue Binding!!',
+      htmlString: '<p style="color:red">color</p>',
+      checked: ''
+    }
+  }
+})
+</script>
