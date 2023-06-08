4c7aa0de2c6702d8d2df31cadcba3c2dd29800eb
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2023-06-08 20:28:51 +0900

    Add files via upload

---
+++ project-ex/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <PageHeader/>
+    <PageMenu/>
+    <PageHome/>
+  </div>
+  <PageFooter/>
+</template>
+<script>
+import PageFooter from '@/components/PageFooter.vue'
+import PageHeader from './components/PageHeader.vue'
+import PageMenu from './components/PageMenu.vue'
+import PageHome from './components/PageHome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageFooter,
+    PageHeader,
+    PageMenu,
+    PageHome
+  }
+}
+</script>
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+a{
+  text-align: right;
+  border-color: aliceblue;
+}
+</style>
